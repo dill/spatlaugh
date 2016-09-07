32d87fccd62f7c98296d342f21d85f39b6d984af
David Lawrence Miller <github@ninepointeightone.net>
2016-09-07 16:53:23 -0400

    well better think about that some more...

--- simulation_study/results.R
+++ simulation_study/results.R
@@ -1,29 +1,37 @@
-library(ggplot2)
 library(plyr)
 
 # one for each distribution/detectability
-f_names <- list.files(pattern="*corner-covar\\.RData")
+f_names <- list.files(path="../simulation_study/",  pattern="*\\.RData", 
+                      full.names=TRUE)
 
 # lookup for model names to short descriptions
-model_name_lookup <- list("HT" = "Horvitz-Thompson",
-                          "HT_strat" = "Horvitz-Thompson (stratified)",
-                          "m_xy_ds"  = "Duchon spline bivariate",
-                          "m_xy_te"  = "Thin plate tensor product",
-                          "m_xy_tp"  = "Thin plate bivariate",
-                          "m_xy_ts"  = "Thin plate with shrinkage bivariate",
-                          "m_xyr_tp"  = "Thin plate bivariate rotated",
-                          "m_xyr_te"  = "Thin plate tensor product rotated"
-                          )
+model_name_lookup <- c("HT",
+                       "HTcovar",
+                       "HTstrat",
+                       "HTstratcovar",
+                       "Duchon",
+                       "TPTE",
+                       "TP",
+                       "TPSH",
+                       "TPR",
+                       "TPTER")
 
-# helper functions
-reasonable_range <- function(x){
-  # 1 and 5 are whiskers
-  boxplot.stats(x)$stats[c(1,5)]
-}
-clipped_obs <- function(x, rr, name){
-  x <- x[[name]]
-  length(x[x<=rr[1] | x>=rr[2]])
-}
+names(model_name_lookup) <- c("HT",
+                              "HT_cov",
+                              "HT_strat",
+                              "HT_strat_cov",
+                              "m_xy_ds",
+                              "m_xy_te",
+                              "m_xy_tp",
+                              "m_xy_ts",
+                              "m_xyr_tp",
+                              "m_xyr_te")
+model_name_lookup <- factor(model_name_lookup, levels=c("HT", "HTcovar", "HTstrat",
+                                                        "HTstratcovar", "Duchon", "TPTE",
+                                                        "TP", "TPSH", "TPR", "TPTER"))
+
+
+huge_res <- c()
 
 # now loop over the files
 for(i in seq_along(f_names)){
@@ -31,84 +39,32 @@ for(i in seq_along(f_names)){
   load(f_names[i])
 
   # print the name of this scenario as the header
+  f_names[i] <- sub("../simulation_study//", "", f_names[i], fixed=TRUE)
   f_names[i] <- sub(".RData", "", f_names[i])
-  cat("\n\n## ", f_names[i],"\n\n")
+  big_res$scenario <- f_names[i]
+  big_res$model <- as.character(big_res$model)
+  big_res$model <- model_name_lookup[big_res$model]
 
-dev.new()
+  ## WARNING: magic numbers below!!
   # bias boxplot
   big_res$bias <- big_res$N-500
-  p <- ggplot(big_res)+
-    geom_boxplot(aes(model, bias)) +
-    #facet_wrap(~names, scales="free_x",nrow=2)+
-    ggtitle(paste0(f_names[i], "Bias")) +
-    labs(y="Bias") +
-    coord_cartesian(ylim=c(-500, 500)) +
-    theme_minimal() +
-    geom_hline(aes(yintercept=0), colour="red")
-  print(p)
 
-dev.new()
-  # Mark's fancy quantile diagnostic, described above
-  p <- ggplot(big_res)+
-    geom_histogram(aes(quantile))+
-    facet_wrap(~model, nrow=2)+
-    ggtitle(paste0(f_names[i], "Quantile diagnostic")) +
-    geom_vline(xintercept=0.5) +
-    theme_minimal()
-  print(p)
+  # separate out the scenario definition
+  big_res$density <- sub("([a-z]+)-[a-z]+-[a-z]+", "\\1", big_res$scenario[1])
+  big_res$design <- sub("[a-z]+-([a-z]+)-[a-z]+", "\\1", big_res$scenario[1])
 
-  #rr <- reasonable_range(big_res$sen)
-  ## build a data.frame to count clipped observation
-  #clipped_d <- ddply(big_res, .(names), clipped_obs, name="sen", rr=rr)
-  #p <- ggplot(big_res)+
-  #  geom_histogram(aes(sen))+
-  #  facet_wrap(~names, scales="free_x",nrow=2)+
-  #  scale_x_continuous(limits = rr) +
-  #  geom_text(aes(label=V1, x=rr[1]+(rr[2]-rr[1]), y=50 ), data=clipped_d) +
-  #  ggtitle(paste(f_names[i], "se/sqrt(n)"))
-  #print(p) 
-  #
-  ## smoothing parameters
-  #sp_dat <- big_res[!(big_res$names %in% c("HT","HT_strat")),]
-  #sp_dat$names <- as.character(sp_dat$names)
-  #
-  ## mash the sp2s into the sp1 data
-  #sp_dat2 <- big_res[big_res$names %in% c("m_xy_te","m_xyr_te"),]
-  #sp_dat2$names <- as.character(sp_dat2$names)
-  #sp_dat2$names[sp_dat2$names=="m_xy_te"] <- "m_xy_te sp2"
-  #sp_dat2$names[sp_dat2$names=="m_xyr_te"] <- "m_xyr_te sp2"
-  #sp_dat <- rbind(sp_dat,sp_dat2)
-  #sp_dat$names <- as.factor(sp_dat$names)
-  #
-  #sp_dat$names <- droplevels(sp_dat$names)
-  #rr <- reasonable_range(sp_dat$sp1)
-  ## build a data.frame to count clipped observation
-  #clipped_d <- ddply(sp_dat, .(names), clipped_obs, name="sp1", rr=rr)
-  #p <- ggplot(sp_dat)+
-  #  geom_histogram(aes(sp1))+
-  #  facet_wrap(~names, scales="free_x",nrow=2)+
-  #  ggtitle(paste(f_names[i], "1/sp1"))
-  #print(p) 
-  
+  if(big_res$design == "tb"){
+	big_res$design <- "twozigzag"
+  }
+
+  if(!grepl("covar", big_res$scenario[1])){
+    big_res$df <- sub("[a-z]+-[a-z]+-([a-z]+)", "\\1", big_res$scenario[1])
+    big_res$covar <- FALSE
+  }else{
+    big_res$covar <- TRUE
+	big_res$df <- "covar"
+  }
+
+  huge_res <- rbind(huge_res, big_res)
 }
-
-
-## Number of detections per simulation
-
-#ndat <- c()
-#for(i in seq_along(f_names)){
-#  load(f_names[i])
-#  ndat <- rbind(ndat,
-#                cbind(scenario=sub(".RData", "", f_names[i]),
-#                      big_res[big_res$name=="HT",])
-#                )
-#}
-#p <- ggplot(ndat)+
-#  geom_histogram(aes(n), binwidth=2)+
-#  ggtitle("detections") +
-#  facet_wrap(~scenario, nrow=1) +
-#  theme_minimal()
-#print(p) 
-
-
 
